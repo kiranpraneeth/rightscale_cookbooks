b54630e0646f01558e2dd29b0c33b63574f818f1
Kiran Praneeth Gamini <kiran@Opstoolsqa-2.(none)>
2012-06-13 07:42:28 -0700

    Adding list of packages to adroit

--- cookbooks/app_adroit/recipes/default.rb
+++ cookbooks/app_adroit/recipes/default.rb
@@ -1,9 +1,7 @@
 #
-# Cookbook Name:: app_tomcat
+# Cookbook Name:: app_adroit
 #
-# Copyright RightScale, Inc. All rights reserved.  All access and use subject to the
-# RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
-# if applicable, other agreements such as a RightScale Master Subscription Agreement.
+# Copyright Inmobi, Inc. All rights reserved.
 
 rightscale_marker :begin
 
@@ -18,38 +16,177 @@ when "ubuntu", "debian"
   when "backend"
     log "Entered backend type case"
     node[:app][:packages] = [
-      " roi-tracker-cron-package"
-      #"roi-feedback-package"
-      #"java-gcj-compat-dev",
+    "roi-tracker-cron-package",
+    "roi-feedback-package",
+    "inmobi-ivory-client"
+    "inmobi-adroit-collectdconf",
+    "inmobi-apptracker-backfill-cron",
+    "inmobi-daemontools",
+    "inmobi-dnscache",
+    "inmobi-loop",
+    "inmobi-mcollective-plugins",
+    "inmobi-ntpd",
+    "inmobi-snmpd",
+    "inmobi-tinydns",
+    "libapparmor-perl",
+    "libbit-vector-perl",
+    "libcarp-clan-perl",
+    "libclass-accessor-perl",
+    "libcommon-sense-perl",
+    "libconfig-general-perl",
+    "libconfig-tiny-perl",
+    "libdate-manip-perl",
+    "libdbd-pg-perl",
+    "libdbi-perl",
+    "liberror-perl",
+    "libfont-afm-perl",
+    "libhtml-format-perl",
+    "libhtml-parser-perl",
+    "libhtml-tagset-perl",
+    "libhtml-tree-perl",
+    "libio-pty-perl",
+    "libio-string-perl",
+    "libipc-run-perl",
+    "libjson-perl",
+    "libjson-xs-perl",
+    "liblocale-gettext-perl",
+    "liblog-dispatch-filerotate-perl",
+    "liblog-dispatch-perl",
+    "liblog-log4perl-perl",
+    "libmailtools-perl",
+    "libmath-calc-units-perl",
+    "libnagios-plugin-perl",
+    "libnet-daemon-perl",
+    "libparams-validate-perl",
+    "libparse-debianchangelog-perl",
+    "libperl5.10",
+    "libplrpc-perl",
+    "libproc-daemon-perl",
+    "librpc-xml-perl",
+    "libsub-name-perl",
+    "libterm-readkey-perl",
+    "libtext-charwidth-perl",
+    "libtext-glob-perl",
+    "libtext-iconv-perl",
+    "libtext-wrapi18n-perl",
+    "libtime-modules-perl",
+    "libtimedate-perl",
+    "liburi-perl",
+    "libwww-perl",
+    "libxml-libxml-perl",
+    "libxml-namespacesupport-perl",
+    "libxml-parser-perl",
+    "libxml-sax-expat-perl",
+    "libxml-sax-perl",
+    "libxml-simple-perl",
+    "libyaml-perl",
+    "libyaml-syck-perl",
+    "perl",
+    "perl-base",
+    "perl-doc",
+    "perl-modules",
+    "puppet-metadata-plugin"
     ]
   when "frontend"
+    log "Entered frontend type case"
     node[:app][:packages] = [
-      #"java-gcj-compat-dev",
+    "com.mkhoj.datalogger-mkhoj-data_logger-",
+    "com.mkhoj.util-mkhoj-msisdn-lib",
+    "download-tracker.phoenix.worker-initwor",
+    "download-tracker.phoenix.worker-respons",
+    "inmobi-adroit-integration",
+    "inmobi-apptracker-phoenix-conf",
+    "inmobi-download-server-conf",
+    "inmobi-narada-keys",
+    "inmobi-service-narada",
+    "mkhoj-analytics-beacon-server",
+    "nginx-johndoe",
+    "phoenix-libjava-com.googlecode.json-sim",
+    "phoenix-libjava-commons-beanutils-commo",
+    "phoenix-libjava-commons-beanutils-commo",
+    "phoenix-libjava-commons-collections-com",
+    "phoenix-libjava-commons-configuration-c",
+    "phoenix-libjava-commons-digester-common",
+    "phoenix-libjava-commons-lang-commons-la",
+    "phoenix-libjava-commons-logging-commons",
+    "phoenix-libjava-junit-junit",
+    "phoenix-libjava-log4j-log4j",
+    "phoenix-libjava-net.sourceforge.wurfl-w",
+    "phoenix-libjava-org.json-json",
+    "phoenix-libjava-org.springframework-spr",
+    "phoenix-libjava-postgresql-postgresql",
+    "phoenix.core-phoenix",
+    "phoenix.data-data",
+    "phoenix.entity-ccid",
+    "phoenix.entity-handset",
+    "phoenix.tracker-download-tracker",
+    "phoenix.transporter-transporter",
+    "phoenix.util-util",
+    "phoenix.worker-jsonlogger",
+    "phoenix.workergroup-workergroup",
+    "inmobi-loop",
+    "libapparmor-perl",
+    "libclass-accessor-perl",
+    "libdate-manip-perl",
+    "liberror-perl",
+    "libfont-afm-perl",
+    "libhtml-format-perl",
+    "libhtml-parser-perl",
+    "libhtml-tagset-perl",
+    "libhtml-tree-perl",
+    "libio-pty-perl",
+    "libio-string-perl",
+    "libipc-run-perl",
+    "liblocale-gettext-perl",
+    "liblog-dispatch-filerotate-perl",
+    "liblog-dispatch-perl",
+    "liblog-log4perl-perl",
+    "libmailtools-perl",
+    "libparams-validate-perl",
+    "libparse-debianchangelog-perl",
+    "libperl5.10",
+    "librpc-xml-perl",
+    "libsub-name-perl",
+    "libterm-readkey-perl",
+    "libtext-charwidth-perl",
+    "libtext-glob-perl",
+    "libtext-iconv-perl",
+    "libtext-wrapi18n-perl",
+    "libtimedate-perl",
+    "liburi-perl",
+    "libwww-perl",
+    "libxml-libxml-perl",
+    "libxml-namespacesupport-perl",
+    "libxml-parser-perl",
+    "libxml-sax-expat-perl",
+    "libxml-sax-perl",
+    "libxml-simple-perl",
+    "libyaml-perl",
+    "libyaml-syck-perl",
+    "perl",
+    "perl-base",
+    "perl-doc",
+    "perl-modules",
     ]
   when "gboconsole"
+    log "Entered frontend type case"
     node[:app][:packages] = [
-      #"java-gcj-compat-dev",
+      "adroit-console",
+      "gbo-script"
     ]
   else
-    raise "Unrecognized database adapter #{node[:app_tomcat][:db_adapter]}, exiting "
+    raise "Unrecognized function #{node[:app_adroit][:function]}, exiting "
   end
 else
   raise "Unrecognized distro #{node[:platform]}, exiting "
 end
 
+#Using app cookbook and installing the required packages using APT
 app "default" do
   persist true
   provider node[:app][:provider]
   packages node[:app][:packages]
   action :install
 end
-# Setting app LWRP attribute
-#node[:app][:root] = "#{node[:repo][:default][:destination]}/#{node[:web_apache][:application_name]}"
-# tomcat shares the same doc root with the application destination
-#node[:app][:destination]="#{node[:app][:root]}"
-
-#directory "#{node[:app][:destination]}" do
-#  recursive true
-#end
 
 rightscale_marker :end
